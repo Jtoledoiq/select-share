a2fd9016d9312fb7de5e835f1c2716085058c91d
jtoledoiq <jtoledoiq@gmail.com>
2020-11-05 13:16:19 +0100

    fetch value of the input

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+const form = document.querySelector('form')!
+const formInput =form.querySelector('#address')! as HTMLInputElement
+
+function renderMap(event: Event): any {
+    event.preventDefault();
+    const enteredAddress= formInput.value;
+
+    //send enteredAddress to Google's API
+}
+
+form.addEventListener('submit', renderMap)
