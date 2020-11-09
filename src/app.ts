f8fe5576186c087163b74ef282d8f70aee4a1b4d
jtoledoiq <jtoledoiq@gmail.com>
2020-11-09 10:54:30 +0100

    add google api and finish project

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,43 @@
-const form = document.querySelector('form')!
-const formInput =form.querySelector('#address')! as HTMLInputElement
+import axios from "axios";
 
-function renderMap(event: Event): any {
-    event.preventDefault();
-    const enteredAddress= formInput.value;
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
 
-    //send enteredAddress to Google's API
+const GOOGLE_API_KEY = "AIzaSyCIaAc2c5M3VpbCH6PPq_guwy9lHuowXOs";
+
+ declare var google: any;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then(response => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: coordinates,
+        zoom: 16
+      }); 
+
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch(err => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
-form.addEventListener('submit', renderMap)
+form.addEventListener("submit", searchAddressHandler);
